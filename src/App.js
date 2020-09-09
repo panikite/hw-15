272ece9588081e201f49d99d863e8f29601c48a4
panikite <ne.isnuje@gmail.com>
2020-09-09 12:32:28 +0300

    add hw#15

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import Post from '../src/component/post';
+
+const ANAKIN_IMAGE = "https://upload.wikimedia.org/wikipedia/en/thumb/7/74/Anakin-Jedi.jpg/220px-Anakin-Jedi.jpg";
+
+const RAY_IMAGE = "https://specials-images.forbesimg.com/imageserve/5e63b3c8e1e617000759130e/960x0.jpg?fit=scale";
+
+
+
+function App() {
+
+  return (
+    <div className="post_wrapper">
+      <Post author={{
+                name: "Anakin skywalker",
+                photo: ANAKIN_IMAGE,
+                nickname: "@dart_vader"
+            }}
+            content="WTF? Who is Ray? Why she is Skywalker? Luke...?"
+            image={RAY_IMAGE}
+            date={"26 февр."}
+            comments={482}
+            reposts={146}
+            likes={887}
+      />
+    </div>
+  );
+}
+
+export default App;
