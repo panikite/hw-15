c12eca8ad8043c2702b23be7254419b3922e89a1
panikite <ne.isnuje@gmail.com>
2020-09-09 12:36:09 +0300

    Updates

---
+++ precache-manifest.4e897183aca5a80e29e8cf1d4e3596d4.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "529446d5f32a7a03dd9cb29c3bbd60e0",
+    "url": "/hw-15/index.html"
+  },
+  {
+    "revision": "65fcd91e0e303534cec5",
+    "url": "/hw-15/static/css/main.391dc913.chunk.css"
+  },
+  {
+    "revision": "923017498649d193c53d",
+    "url": "/hw-15/static/js/2.58817b4d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hw-15/static/js/2.58817b4d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65fcd91e0e303534cec5",
+    "url": "/hw-15/static/js/main.cd812e2a.chunk.js"
+  },
+  {
+    "revision": "6635940cc4682222d79b",
+    "url": "/hw-15/static/js/runtime-main.30475018.js"
+  },
+  {
+    "revision": "08ace6deca84711b2d850b6c20735456",
+    "url": "/hw-15/static/media/comment.08ace6de.svg"
+  },
+  {
+    "revision": "4a182492b62b951a851efe983152f2f1",
+    "url": "/hw-15/static/media/heart.4a182492.svg"
+  },
+  {
+    "revision": "12f33f8aaf9dfc3884b8e039e410a7ff",
+    "url": "/hw-15/static/media/retweet.12f33f8a.svg"
+  },
+  {
+    "revision": "535a86906541408adfbc771af4fae7a9",
+    "url": "/hw-15/static/media/share.535a8690.svg"
+  }
+]);
